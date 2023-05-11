3fe2786b51b02cba3f56ffe57580426061e30b22
rkdekal <129129508+rkdekal@users.noreply.github.com>
2023-05-11 01:19:17 +0000

    switch, break에 대해 배움

---
+++ .ccls-cache/@home@runner@damiyi-gasangpoldeo/condition-elseif.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// main(){
+// int a, b;
+// printf("서로 다른 두 개의 정수를 입력하세요.\n");
+// printf("첫번째 정수를 입력:");
+// scanf("%d", &a);
+// printf("두번째 정수를 입력:");
+// scanf("%d", &b);
+// printf("첫번째 입력된 정수는 %d 이고 두번째 입력된 정수는 %d이며 두 개 중 큰 수는 %d 입니다.\n", a, b, max(a, b));
+// }
+// int max(int a, int b)
+// {
+// if (a > b)
+// return a;
+// else
+// return b;
+// }
